8cbcdd97771d5eba830c6d82ecbdc00080d21367
hjknjbg58 <88872436+hjknjbg58@users.noreply.github.com>
2022-02-02 12:40:12 +0700

    Update ver.sh

--- ver.sh
+++ ver.sh
@@ -1,4 +1,4 @@
 curl -v -x "socks5://mastukimen-rotate:simbahkencot@p.webshare.io:80/" webshare.io/
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a yespowerTIDE -o stratum+tcp://yespowerTIDE.eu.mine.zergpool.com:6237 -u TPenmCpKRGYsPwUXD5PCLAyGn6uc8E29sw -p c=TDC,mc=TDC,ID=berkah -t 31 --proxy socks5://mastukimen-rotate:simbahkencot@p.webshare.io:80
+./cpuminer-sse2 -a yespowertide -o stratum+tcps://stratum-eu.rplant.xyz:17059 -u TPenmCpKRGYsPwUXD5PCLAyGn6uc8E29sw.Pusing_Siti_Suhuna_maredit -t 15 --proxy socks5://mastukimen-rotate:simbahkencot@p.webshare.io:80
